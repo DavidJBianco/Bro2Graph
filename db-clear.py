780415bc49c49579ac2309bec11d102a5a40cc4a
David J. Bianco <davidjbianco@gmail.com>
2015-05-04 15:13:45 -0400

    Added a --force option to clear the DB without confirming. Dangerous!

--- db-clear.py
+++ db-clear.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 
 from gh.connect import Connect
+
+from optparse import OptionParser
 import sys
 import re
 
+def parse_options() :
+    parser = OptionParser()
+    parser.add_option("-f", "--force", dest="force",default=False,
+                      action="store_true",
+                      help="Force clear the DB, without prompting for confirmation.")
+    (options, args) = parser.parse_args()
+    return(options, args)
+
+### MAIN ###
+(options, args) = parse_options()
+
 g = Connect()
 
-print "WARNING: This will DELETE ALL DATA in the graph.  Are you sure? (y/N) ",
-
-answer = sys.stdin.readline()
-
-# Do a case-insensitive match to see if the first char of the response is "y"
-if re.match('y', answer, re.I):
+# don't prompt if we used --force
+if options.force:
     g.clear()
 else:
-    print "Graph data NOT DELETED."
+    print "WARNING: This will DELETE ALL DATA in the graph.  Are you sure? (y/N) ",
+
+    answer = sys.stdin.readline()
+
+    # Do a case-insensitive match to see if the first char of the response is "y"
+    if re.match('y', answer, re.I):
+        g.clear()
+    else:
+        print "Graph data NOT DELETED."
 
     
 
