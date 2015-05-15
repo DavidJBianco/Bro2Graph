9fb18e35b5cca6bf8f65101cbe916173d8aba1ec
David J. Bianco <davidjbianco@gmail.com>
2015-05-15 11:33:47 -0400

    Initial checking of a util to read all nodes & edges from the graph and stream them to Gephi for visualization.

---
+++ db-graph.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+from gh.connect import Connect
+import gh
+
+from GephiStreamer import Node, Edge, GephiStreamerManager
+
+from random import choice
+
+def display_graph(t, nodes, edges):
+
+    if nodes != None:
+        print "*** Graphing Nodes"
+        for n in nodes:
+            node_temp = Node(n._id)
+
+            properties = n.map()
+            for key in properties:
+                node_temp.property[key] = properties[key]
+
+            node_temp.property["colour"] = node_temp.property["color"]
+            t.add_node(node_temp)
+
+    if edges != None:
+        print "*** Graphing Edges"
+        for e in edges:
+            src = e._outV
+            dst = e._inV
+            edge_temp = Edge(src, dst, directed=True)
+
+            properties = e.map()
+            for key in properties:
+                edge_temp.property[key] = properties[key]
+            
+            t.add_edge(edge_temp)
+
+    t.commit()
+
+def display_graph_from_list(t, l=None):
+    if l == None:
+        return
+
+    for element in l:
+        if element["_type"] == "vertex":
+            node_temp = Node(element["_id"])
+
+            for key in element:
+                node_temp.property[key] = element[key]
+            node_temp.property["colour"] = node_temp.property["color"]
+
+            t.add_node(node_temp)
+        elif element["_type"] == "edge":
+            src = element["_outV"]
+            dst = element["_inV"]
+            edge_temp = Edge(src, dst, directed=True)
+
+            for key in element:
+                edge_temp.property[key] = element[key]
+
+            t.add_edge(edge_temp)
+
+    t.commit()
+
+
+g = Connect()
+t = GephiStreamerManager()
+
+print "Getting nodes..."
+nodes = g.V
+print "Getting edges..."
+edges = g.E
+
+display_graph(t, g.V, g.E)
+
