9d8ab256ab25f055bb6a06f65cd645664bc69177
David J. Bianco <davidjbianco@gmail.com>
2015-05-29 16:21:00 -0400

    Instead of two node IDs, you can optionally specify the second arg as a node type (value of "element_type" feature).  This would let find a path to ANY node of that type.
    Also upped the default search depth from 4 to 5.

--- db_path.py
+++ db_path.py
@@ -2,6 +2,7 @@
 
 from optparse import OptionParser
 import sys
+import re
 
 import gh
 
@@ -13,7 +14,7 @@ def parse_options() :
     parser.add_option("-d", "--directed", dest="directed", default=False,
                       action="store_true",
                       help="Respect relationship direction when finding paths.")
-    parser.add_option("-m", "--max_hops", dest="max_hops", default=4,
+    parser.add_option("-m", "--max_hops", dest="max_hops", default=5,
                       help="Max number of hops in path.")
     (options, args) = parser.parse_args()
     return(options, args)
@@ -23,14 +24,25 @@ def parse_options() :
 (options, args) = parse_options()
 
 if len(args) != 2:
-    print "ERROR: You must specify both the beginning and ending node IDs"
+    print "ERROR: You must specify both the beginning and ending nodes."
     sys.exit(-1)
 
 g = gh.connect.Connect()
 
-res = gh.util.shortest_path(g, args[0], args[1],
-                            directed=options.directed,
-                            max_hops=options.max_hops)
+src = args[0]
+dst = args[1]
+
+# Look up the source ID, if we gave a name
+if re.match("\d+$", dst):
+    print "Looking up by destination vertex ID"
+    lookup = gh.util.shortest_path
+else:
+    print "Looking up by destination vertex type"
+    lookup = gh.util.shortest_path_to_type
+
+res = lookup(g, src, dst,
+             directed=options.directed,
+             max_hops=options.max_hops)
 
 if res:
     for r in res:
